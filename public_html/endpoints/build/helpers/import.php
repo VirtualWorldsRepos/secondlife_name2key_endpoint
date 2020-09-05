5502b8ec88ae0dcc37e503daee9a0ef4c2cadb1c
Madpeter <madpeter@gmail.com>
2020-09-06 00:36:04 +0100

    makes the import save more often

--- public_html/endpoints/build/helpers/import.php
+++ public_html/endpoints/build/helpers/import.php
@@ -4,10 +4,12 @@ ini_set('memory_limit', '8095M');
 if(isset($_GET["page"]) == true)
 {
     $page = $_GET["page"];
+    $all_ok = true;
     if(($page >= 0) && ($page <= 5))
     {
         output("importing dataset ".($page+1)." of 6<br/>");
         $group_options[] = "other";
+        $counter = 0;
         foreach($group_options as $option)
         {
             set_time_limit (480);
@@ -17,6 +19,16 @@ if(isset($_GET["page"]) == true)
                 output("group ".$option."/".$subgroups." -");
                 output(print_r($sql->RawSQL($import_file),true));
                 output(" DONE <br/>");
+            }
+            $counter++;
+            if($counter==3)
+            {
+                $counter = 0;
+                output("<br/>Saving now please wait");
+                if($sql->sqlSave() == false)
+                {
+                    $all_ok = false;
+                }
             }
         }
         sleep(1);
